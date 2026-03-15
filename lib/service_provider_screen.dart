d66dc64322550ca6baafe8ffb91e37bb88f7d5c9
Raja <rajakaveripalayam@gmail.com>
2026-03-15 23:50:56 +0100

    display list of skills in service provider screen

--- lib/service_provider_screen.dart
+++ lib/service_provider_screen.dart
@@ -1,30 +1,56 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:em_poverty/uicomponent/multiple_choice_view.dart';
 import 'package:flutter/material.dart';
 
-@RoutePage()
-class ServiceProviderScreen extends  StatelessWidget {
+class ServiceProviderScreen extends StatefulWidget {
   const ServiceProviderScreen({super.key});
+
+  @override
+  State<ServiceProviderScreen> createState() => _ServiceProviderScreenState();
+}
+
+class _ServiceProviderScreenState extends State<ServiceProviderScreen> {
+  final List<String> _allOptions = [
+    "Plumber",
+    "Painter",
+    "Electrician",
+    "Carpenter",
+    "Cleaning Worker",
+  "Ac Worker"];
+
+  // THE SINGLE SOURCE OF TRUTH
+  List<String> _mySelectedSkills = [];
+
+  void _handleSkillToggle(String skill, bool isSelected) {
+    setState(() {
+      if (isSelected) {
+        _mySelectedSkills.add(skill);
+      } else {
+        _mySelectedSkills.remove(skill);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme
-            .of(context)
-            .colorScheme
-            .inversePrimary,
-        title: Text("Home Page"),
-      ),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Welcome Provider"),
-            ],
+      body: SafeArea(child:
+      Column(
+        children: [
+          MultipleChoiceView(
+            allSkills: _allOptions,
+            selectedSkills: _mySelectedSkills, // Pass data DOWN
+            onToggle: _handleSkillToggle,      // Pass event UP
           ),
-        ),
+          ElevatedButton(
+            onPressed: () => print("Saving: $_mySelectedSkills"),
+            child: const Text("Save"),
+          )
+        ],
       ),
+      )
+
+
     );
   }
 }
---
+++ lib/uicomponent/multiple_choice_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MultipleChoiceView extends StatelessWidget {
+  final List<String> allSkills;
+  final List<String> selectedSkills;
+  final Function(String, bool) onToggle; // Tells parent WHICH skill and if it was selected
+
+  const MultipleChoiceView({
+    super.key,
+    required this.allSkills,
+    required this.selectedSkills,
+    required this.onToggle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 8.0,
+      children: allSkills.map((skill) {
+        return FilterChip(
+          label: Text(skill),
+          selected: selectedSkills.contains(skill),
+          onSelected: (bool selected) {
+            onToggle(skill, selected); // Just pass the event up
+          },
+        );
+      }).toList(),
+    );
+  }
+}
